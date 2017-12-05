20eeece51aec41534cf65cf5febbbb979a712aeb
Naga Harshini Voruganti <nvorugan@comet-ln3.sdsc.edu>
2017-12-04 22:59:29 -0800

    Code Submission

--- helper.cpp
+++ helper.cpp
@@ -23,71 +23,71 @@ void printMat(const char mesg[], double *E, int m, int n);
 
 
 void localInit(double *E, double *E_prev, double *R, int m, int n,
-	       int yNum, int xNum, int dim_y, int dim_x){
-  // yNum start point of this subgrid
-  // xNum start point of this subgrid
-  // dim_y - dimensions of subgrid in y (row) direction (prepad) 
-  // dim_x - dimensions of subgrid in x (col) direction (prepad)
-  int absMidPt = (n+1)/2;
-  int local_lo = xNum;
-  int local_hi = local_lo + dim_x;
-  int i;
+		int yNum, int xNum, int dim_y, int dim_x){
+	// yNum start point of this subgrid
+	// xNum start point of this subgrid
+	// dim_y - dimensions of subgrid in y (row) direction (prepad) 
+	// dim_x - dimensions of subgrid in x (col) direction (prepad)
+	int absMidPt = (n+1)/2;
+	int local_lo = xNum;
+	int local_hi = local_lo + dim_x;
+	int i;
 
-  //  printf("localInit absMidPt = %d, local_lo = %d\n", absMidPt, local_lo);
-  if (absMidPt > local_hi){
-    for (i=0; i<(dim_x+2)*(dim_y+2); i++){
-      E_prev[i] = 0.0;
-    }
-    
-  }else if (absMidPt <= local_lo){
-    for (i=0; i<(dim_x+2)*(dim_y+2); i++){
-      E_prev[i] = 1.0;
-    }
+	//  printf("localInit absMidPt = %d, local_lo = %d\n", absMidPt, local_lo);
+	if (absMidPt > local_hi){
+		for (i=0; i<(dim_x+2)*(dim_y+2); i++){
+			E_prev[i] = 0.0;
+		}
 
-  }else{
-    for (i=0; i<(dim_x+2)*(dim_y+2); i++){
-      E_prev[i] = R[i] = 0.0;
-    }
-    int local_mid = absMidPt - local_lo;
-    for (i=0; i<(dim_x+2)*(dim_y+2); i++){
-      int colIndex = i % (dim_x+2);
-      if (colIndex == 0 || colIndex == (dim_x+1) || colIndex < (local_mid+1)){
-	continue;
-      }
-      E_prev[i] = 1.0;
-    }
-  }
+	}else if (absMidPt <= local_lo){
+		for (i=0; i<(dim_x+2)*(dim_y+2); i++){
+			E_prev[i] = 1.0;
+		}
+
+	}else{
+		for (i=0; i<(dim_x+2)*(dim_y+2); i++){
+			E_prev[i] = R[i] = 0.0;
+		}
+		int local_mid = absMidPt - local_lo;
+		for (i=0; i<(dim_x+2)*(dim_y+2); i++){
+			int colIndex = i % (dim_x+2);
+			if (colIndex == 0 || colIndex == (dim_x+1) || colIndex < (local_mid+1)){
+				continue;
+			}
+			E_prev[i] = 1.0;
+		}
+	}
 
 
-  absMidPt = (m+1)/2;
-  local_lo= yNum;
-  local_hi = local_lo + dim_y;
-  if (absMidPt > local_hi){
-    for (i=0; i<(dim_x+2)*(dim_y+2); i++){
-      R[i] = 0.0;
-    }
-  }else if (absMidPt <= local_lo){
-    for (i=0; i<(dim_x+2)*(dim_y+2); i++){
-      R[i] = 1.0;
-    }
-  }else{
-    for (i=0; i<(dim_x+2)*(dim_y+2); i++){
-      R[i] = 0.0;
-    }
-    int local_mid = absMidPt - local_lo;
-    for (i=0; i<(dim_x+2)*(dim_y+2); i++){
-      int rowIndex = i / (dim_x+2);
-      int colIndex = i % (dim_x+2);
-      if ((colIndex == 0) || (colIndex == (dim_x+1)) || (rowIndex < (local_mid+1))){
-	continue;
-      }
-      R[i] = 1.0;
-    }
-  }
+	absMidPt = (m+1)/2;
+	local_lo= yNum;
+	local_hi = local_lo + dim_y;
+	if (absMidPt > local_hi){
+		for (i=0; i<(dim_x+2)*(dim_y+2); i++){
+			R[i] = 0.0;
+		}
+	}else if (absMidPt <= local_lo){
+		for (i=0; i<(dim_x+2)*(dim_y+2); i++){
+			R[i] = 1.0;
+		}
+	}else{
+		for (i=0; i<(dim_x+2)*(dim_y+2); i++){
+			R[i] = 0.0;
+		}
+		int local_mid = absMidPt - local_lo;
+		for (i=0; i<(dim_x+2)*(dim_y+2); i++){
+			int rowIndex = i / (dim_x+2);
+			int colIndex = i % (dim_x+2);
+			if ((colIndex == 0) || (colIndex == (dim_x+1)) || (rowIndex < (local_mid+1))){
+				continue;
+			}
+			R[i] = 1.0;
+		}
+	}
 
-  // We only print the meshes if they are small enough
-  //  printMat("E_prev",E_prev,dim_y,dim_x);
-  //  printMat("R",R,dim_y,dim_x);
+	// We only print the meshes if they are small enough
+	//  printMat("E_prev",E_prev,dim_y,dim_x);
+	//  printMat("R",R,dim_y,dim_x);
 
 
 }
@@ -101,93 +101,93 @@ void localInit(double *E, double *E_prev, double *R, int m, int n,
 // be mapped to appropriate local indices when parallelizing the code
 //
 void init (double *E,double *E_prev,double *R,int m,int n){
-    int i;
+	int i;
 
-    int extra_x = n % cb.px;
-    int extra_y = m % cb.py;
-    int little_m = m / cb.py;
-    int little_n = n / cb.px;
+	int extra_x = n % cb.px;
+	int extra_y = m % cb.py;
+	int little_m = m / cb.py;
+	int little_n = n / cb.px;
 
-    // first row of the R matrix w/o ghost cells in the global problem
-    int initial_i = lb.pIdx*little_m + min(lb.pIdx, extra_y);
-    // distance of initial_i from the first rows of 1s
-    int rem_rows = min((cb.m + 1)/2 - initial_i, lb.m); 
+	// first row of the R matrix w/o ghost cells in the global problem
+	int initial_i = lb.pIdx*little_m + min(lb.pIdx, extra_y);
+	// distance of initial_i from the first rows of 1s
+	int rem_rows = min((cb.m + 1)/2 - initial_i, lb.m); 
 
-    for (i = 0; i < (lb.m + 2)*(lb.n + 2); i++) {
+	for (i = 0; i < (lb.m + 2)*(lb.n + 2); i++) {
 
-      int rIdx = i / (lb.n + 2);
-      int cIdx = i % (lb.n + 2);
+		int rIdx = i / (lb.n + 2);
+		int cIdx = i % (lb.n + 2);
 
-      if (rIdx == 0 || rIdx == lb.m + 1 || rIdx <= rem_rows || cIdx == 0 || cIdx == lb.n + 1) R[i] = 0.0;
-      else R[i] = 1.0;
-    }
+		if (rIdx == 0 || rIdx == lb.m + 1 || rIdx <= rem_rows || cIdx == 0 || cIdx == lb.n + 1) R[i] = 0.0;
+		else R[i] = 1.0;
+	}
 
-    // first col of the E_prev matrix w/o ghost cells in the global problem
-    int initial_j = lb.pIdy*little_n + min(lb.pIdy, extra_x);
-    // distance of initial_j from the first cols of 1s
-    int rem_cols = min((cb.n + 1)/2 - initial_j, lb.n);
+	// first col of the E_prev matrix w/o ghost cells in the global problem
+	int initial_j = lb.pIdy*little_n + min(lb.pIdy, extra_x);
+	// distance of initial_j from the first cols of 1s
+	int rem_cols = min((cb.n + 1)/2 - initial_j, lb.n);
 
-    for (i = 0; i < (lb.m + 2)*(lb.n + 2); i++) {
+	for (i = 0; i < (lb.m + 2)*(lb.n + 2); i++) {
 
-      int rIdx = i / (lb.n + 2);
-      int cIdx = i % (lb.n + 2);
+		int rIdx = i / (lb.n + 2);
+		int cIdx = i % (lb.n + 2);
 
-      if (cIdx == 0 || cIdx == lb.n + 1 || cIdx <= rem_cols || rIdx == 0 || rIdx == lb.m + 1) E_prev[i] = 0.0;
-      else E_prev[i] = 1.0;
-    }
+		if (cIdx == 0 || cIdx == lb.n + 1 || cIdx <= rem_cols || rIdx == 0 || rIdx == lb.m + 1) E_prev[i] = 0.0;
+		else E_prev[i] = 1.0;
+	}
 
-    // We only print the meshes if they are small enough
+	// We only print the meshes if they are small enough
 #if 0
-    printMat("E_prev",E_prev,lb.m,lb.n);
-    printMat("R",R,lb.m,lb.n);
+	printMat("E_prev",E_prev,lb.m,lb.n);
+	printMat("R",R,lb.m,lb.n);
 #endif
 }
 
 double *alloc1D(int paddedM,int paddedN){
 #ifdef _MPI_
-    MPI_Comm_rank(MPI_COMM_WORLD,&lb.rank);
+	MPI_Comm_rank(MPI_COMM_WORLD,&lb.rank);
 #endif
-    
-    lb.pIdx = lb.rank / cb.px;
-    lb.pIdy = lb.rank % cb.px;
 
-    int m = paddedM - 2;
-    int n = paddedN - 2;
+	lb.pIdx = lb.rank / cb.px;
+	lb.pIdy = lb.rank % cb.px;
 
-    // no of cells in y direction
-    lb.m = m / cb.py + (lb.pIdx < (m % cb.py));
-    // no of cells in x direction
-    lb.n = n / cb.px + (lb.pIdy < (n % cb.px)); 
+	int m = paddedM - 2;
+	int n = paddedN - 2;
 
-    lb.recv_W = new double[4*lb.m];
-    lb.recv_E = lb.recv_W + lb.m;
-    lb.send_W = lb.recv_E + lb.m;
-    lb.send_E = lb.send_W + lb.m;
-    
-    double *E;
-    // Ensures that allocated memory is aligned on a 16 byte boundary
-    // plus 2 is the padding to accomodate ghost cells.
-    assert(E= (double*) memalign(16, sizeof(double)*(lb.m+2)*(lb.n+2)));
-    return(E);
+	// no of cells in y direction
+	lb.m = m / cb.py + (lb.pIdx < (m % cb.py));
+	// no of cells in x direction
+	lb.n = n / cb.px + (lb.pIdy < (n % cb.px)); 
+
+	lb.recv_W = new double[4*lb.m];
+	lb.recv_E = lb.recv_W + lb.m;
+	lb.send_W = lb.recv_E + lb.m;
+	lb.send_E = lb.send_W + lb.m;
+
+	double *E;
+	// Ensures that allocated memory is aligned on a 16 byte boundary
+	// plus 2 is the padding to accomodate ghost cells.
+	assert(E= (double*) memalign(16, sizeof(double)*(lb.m+2)*(lb.n+2)));
+	return(E);
 }
 
 void printMat(const char mesg[], double *E, int m, int n)
 {
-  if (m > 8)
-  {
-    return;
-  }
-  printf("%s\n", mesg);
+	if (m > 8)
+	{
+		return;
+	}
+	printf("%s\n", mesg);
 
-  for (int i = 0; i < (m + 2)*(n + 2); ++i)
-  {
-    int rIdx = i / (n + 2);
-    int cIdx = i % (n + 2);
+	for (int i = 0; i < (m + 2)*(n + 2); ++i)
+	{
+		int rIdx = i / (n + 2);
+		int cIdx = i % (n + 2);
 
-    // printing along with ghost cells. used for testing.
-    if ((cIdx == 0 || cIdx == n + 1) && (rIdx == 0 || rIdx == m+1)) printf("      ");
-    else printf("%1.3f ", E[i]); 
-    
-    if (cIdx == n+1)printf("\n");
-  }
+		// printing along with ghost cells. used for testing.
+		if ((cIdx == 0 || cIdx == n + 1) && (rIdx == 0 || rIdx == m+1)) printf("      ");
+		else printf("%1.3f ", E[i]); 
+
+		if (cIdx == n+1)printf("\n");
+	}
 }
--- solve.cpp
+++ solve.cpp
@@ -36,7 +36,7 @@ enum { TOP = 0, RIGHT, LEFT, BOTTOM };
 #ifdef SSE_VEC
 // If you intend to vectorize using SSE instructions, you must
 // disable the compiler's auto-vectorizer
-__attribute__((optimize("no-tre_temp_sse-vectorize")))
+__attribute__((optimize("no-tree-vectorize")))
 #endif 
 
 // The L2 norm of an array is computed by taking sum of the squares
@@ -97,46 +97,6 @@ void fill_ghosts(double *E_prev)
 	if (lb.pIdy != RIGHT_EDGE) for (i = (lb.n + CORNER) + (lb.n + PADDING), j = 0; j < lb.m; i += lb.n + PADDING, j++) E_prev[i] = lb.recv_E[j];
 }
 
-// void compute_inner(double *E, double *E_prev, double *R, int start_row, int end_row, int cols, double dt, double alpha) {
-//   int i, j;
-//    double *R_tmp = R;
-//    double *E_tmp = E;
-//    double *E_prev_tmp = E_prev;
-
-//   #ifdef FUSED
-//     // Solve for the excitation, a PDE
-//     for(j = start_row; j <= end_row; j+=(cols+2)) {
-//         E_tmp = E + j;
-//         E_prev_tmp = E_prev + j;
-//         R_tmp = R + j;
-//         for(i = 0; i < cols; i++) {
-//           E_tmp[i] = E_prev_tmp[i]+alpha*(E_prev_tmp[i+1]+E_prev_tmp[i-1]-4*E_prev_tmp[i]+E_prev_tmp[i+(cols+2)]+E_prev_tmp[i-(cols+2)]);
-//           E_tmp[i] += -dt*(kk*E_tmp[i]*(E_tmp[i]-a)*(E_tmp[i]-1)+E_tmp[i]*R_tmp[i]);
-//           R_tmp[i] += dt*(epsilon+M1* R_tmp[i]/( E_tmp[i]+M2))*(-R_tmp[i]-kk*E_tmp[i]*(E_tmp[i]-b-1));
-//         }
-//     }
-//   #else
-//     // Solve for the excitation, a PDE
-//     for(j = start_row; j <= end_row; j+=(cols+2)) {
-//         E_tmp = E + j;
-//         E_prev_tmp = E_prev + j;
-//         for(i = 0; i < cols; i++) {
-//             E_tmp[i] = E_prev_tmp[i]+alpha*(E_prev_tmp[i+1]+E_prev_tmp[i-1]-4*E_prev_tmp[i]+E_prev_tmp[i+(cols+2)]+E_prev_tmp[i-(cols+2)]);
-//         }
-//     }
-
-//     // Solve the ODE, advancing excitation and recovery variables to the next timtestep
-//     for(j = start_row; j <= end_row; j+=(cols+2)) {
-//         E_tmp = E + j;
-//         R_tmp = R + j;
-//         for(i = 0; i < cols; i++) {
-//             E_tmp[i] += -dt*(kk*E_tmp[i]*(E_tmp[i]-a)*(E_tmp[i]-1)+E_tmp[i]*R_tmp[i]);
-//             R_tmp[i] += dt*(epsilon+M1* R_tmp[i]/( E_tmp[i]+M2))*(-R_tmp[i]-kk*E_tmp[i]*(E_tmp[i]-b-1));
-//         }
-//     }
-//   #endif
-// }
-
 void solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Plotter *plotter, double &L2, double &Linf){
 
 	// Simulated time is different from the integer timestep number
@@ -173,13 +133,6 @@ void solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Pl
 	// the desired number of iterations
 	for (niter = 0; niter < cb.niters; niter++) {
 
-		// ***** dunno what this is?
-		// if (cb.debug && (niter==0)) {
-		//   stats(E_prev,m,n,&mx,&sumSq);
-		//   double l2norm = L2Norm(sumSq);
-		//   repNorms(l2norm,mx,dt,m,n,-1, cb.stats_freq);
-		//   if (cb.plot_freq) plotter->updatePlot(E,  -1, m+1, n+1);
-		// }
 		int i,j;
 		if(!cb.noComm) {
 			fill_ghosts(E_prev);
@@ -188,10 +141,8 @@ void solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Pl
 		//////////////////////////////////////////////////////////////////////////////
 
 		//#define FUSED 1
-		// int innerBlockRowStartIndex = 2*((n+2)+1);
-		// int innerBlockRowEndIndex = (((m+2)*(n+2) - 1) - (n)) - 2*(n+2) + 1;
-		// compute_inner(E, E_prev, R, innerBlockRowStartIndex, innerBlockRowEndIndex, n-1, dt, alpha);
 #ifdef SSE_VEC
+
 #ifdef FUSED
 		// Solve for the excitation, a PDE
 		for(j = innerBlockRowStartIndex; j <= innerBlockRowEndIndex; j+=(n+2)) {
@@ -211,8 +162,6 @@ void solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Pl
 			E_tmp = E + j;
 			E_prev_tmp = E_prev + j;
 			//Edits for SSE
-			// for(i = 0; i < n; i++) {
-			//     E_tmp[i] = E_prev_tmp[i]+alpha*(E_prev_tmp[i+1]+E_prev_tmp[i-1]-4*E_prev_tmp[i]+E_prev_tmp[i+(n+2)]+E_prev_tmp[i-(n+2)]);
 			for(i = 0; i < n; i+=4) {
 
 				E_prev_tmp_north = _mm256_loadu_pd(E_prev_tmp + i - (n + 2));
@@ -309,19 +258,12 @@ void solve(double **_E, double **_E_prev, double *R, double alpha, double dt, Pl
 #endif
 		/////////////////////////////////////////////////////////////////////////////////
 
-		// if (cb.stats_freq){
-		//   if ( !(niter % cb.stats_freq)){
-		//      stats(E,m,n,&mx,&sumSq);
-		//      double l2norm = L2Norm(sumSq);
-		//      repNorms(l2norm,mx,dt,m,n,niter, cb.stats_freq);
-		//  }
-		// }
 
-		// if (cb.plot_freq){
-		//        if (!(niter % cb.plot_freq)){
-		//   plotter->updatePlot(E,  niter, m, n);
-		//      }
-		//  }
+		 if (cb.plot_freq){
+		        if (!(niter % cb.plot_freq)){
+		   plotter->updatePlot(E,  niter, m, n);
+		      }
+		  }
 
 		// Swap current and previous meshes
 		double *tmp = E; E = E_prev; E_prev = tmp;
